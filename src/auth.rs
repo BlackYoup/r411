57b4871d1910722bcb400a6c25d10b78a3d54e75
BlackYoup <a.lefebvre@outlook.fr>
2017-04-30 00:26:33 +0200

    login: allow to bypass auth using environment variables

--- src/auth.rs
+++ src/auth.rs
@@ -5,8 +5,13 @@ use rocket::http::{Cookies};
 use user::{User};
 use uuid::{Uuid};
 use std::str::FromStr;
+use std::env;
 
 pub fn authenticate(cookies: &Cookies, state: State<AppState>) -> bool {
+  if env::var("BYPASS_LOGIN").is_ok() {
+    return true;
+  }
+
   let authenticated = is_authenticated(cookies, state);
   if !authenticated && cookies.find("session").is_some() {
     cookies.remove("session");
@@ -29,8 +34,16 @@ fn is_authenticated(cookies: &Cookies, state: State<AppState>) -> bool {
 }
 
 pub fn save_user(state: State<AppState>, token: T411Token) -> User {
-  let uuid = Uuid::new_v4();
-  let user = User::new(uuid, token);
+  let uuid;
+  let user;
+  if env::var("BYPASS_LOGIN").is_ok() {
+    uuid = Uuid::from_str(&env::var("UUID").expect("dev mode: couldn't find UUID env")).unwrap();
+    user = User::new(uuid, env::var("TOKEN").expect("dev mode: couldn't find TOKEN env"));
+  } else {
+    uuid = Uuid::new_v4();
+    user = User::new(uuid, token);
+  }
+
   let mut users = state.users.lock().unwrap();
 
   users.insert(uuid, user.clone());
